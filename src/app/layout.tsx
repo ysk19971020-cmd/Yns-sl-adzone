cc4fef459906fc61589a974696f989107802782a
Yasiru Supun Kavinda <ysk19971020@gmail.com>
2025-12-28 17:43:34 +0000

    බැනර් ඇඩ් පෙන්විමට වෙබ්සයිට් එකෙ උඩ යට සහා වම,දකුන දෙපස ඔගෙ දැන්විම එකතු

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import { Header } from '@/components/layout/header';
 import { Footer } from '@/components/layout/footer';
 import { Toaster } from '@/components/ui/toaster';
 import { FirebaseClientProvider } from '@/firebase/client-provider';
+import Link from 'next/link';
 
 const ptSans = PT_Sans({
   subsets: ['latin'],
@@ -30,13 +31,44 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('min-h-screen bg-background font-body antialiased flex flex-col', ptSans.variable)}>
-        <FirebaseClientProvider>
-          <Header />
-          <main className="flex-1">{children}</main>
-          <Footer />
-          <Toaster />
-        </FirebaseClientProvider>
+      <body className={cn('min-h-screen bg-background font-body antialiased', ptSans.variable)}>
+        <div className="relative flex min-h-screen flex-col">
+          <FirebaseClientProvider>
+            {/* Top Banner */}
+            <div className="banner banner-top">
+              <Link href="/post-ad">
+                <span className="add-your-ad">ඔබේ දැන්වීම එක් කරන්න</span>
+              </Link>
+            </div>
+
+            {/* Left Banner */}
+            <div className="banner banner-left">
+              <Link href="/post-ad">
+                <span className="add-your-ad">ඔබේ දැන්වීම එක් කරන්න</span>
+              </Link>
+            </div>
+            
+            <Header />
+            <main className="flex-1">{children}</main>
+            <Footer />
+
+            {/* Right Banner */}
+            <div className="banner banner-right">
+              <Link href="/post-ad">
+                <span className="add-your-ad">ඔබේ දැන්වීම එක් කරන්න</span>
+              </Link>
+            </div>
+
+            {/* Bottom Banner */}
+            <div className="banner banner-bottom">
+              <Link href="/post-ad">
+                <span className="add-your-ad">ඔබේ දැන්වීම එක් කරන්න</span>
+              </Link>
+            </div>
+
+            <Toaster />
+          </FirebaseClientProvider>
+        </div>
       </body>
     </html>
   );
